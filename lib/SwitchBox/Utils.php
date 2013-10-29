6538a7c5e02b94cb2201edc538b24998f14100bc
Joshua Thijssen <jthijssen@noxlogic.nl>
2013-10-29 13:03:32 +0100

    64byte hashes are converted correctly now

--- lib/SwitchBox/Utils.php
+++ lib/SwitchBox/Utils.php
@@ -12,6 +12,9 @@ class Utils {
         if (function_exists( 'hex2bin')) {
             // When hash starts with leading 0's it gets cut away by PHP during string casting. Hurrah!
             while (strlen($str) < 32) $str = "0" . $str;
+
+            // If we have a 33 byte string (or higher), we assume it's a 64byte hash.
+            while (strlen($str) > 32 && strlen($str) < 64) $str = "0" . $str;
             return hex2bin($str);
         }
 
